b62798e8d984ec9bb0c3bce4575d87e69b95552e
Prajun Adhikary <prajun.adhikary@gmail.com>
2026-08-20 09:45:31 +0545

    Project Name updated

--- src/templates/azoogiDatasheet.js
+++ src/templates/azoogiDatasheet.js
@@ -238,7 +238,7 @@ export const AZOOGI_DATASHEET_HTML = `<section class="page">
         </div>
         {{/if}}
         <div class="note-img-container">
-          <img class="note-img" src="/datasheet-template/assets/ausport.png" alt="Note">
+          <img class="note-img" src="/datasheet-template/assets/auspost.png" alt="Note">
         </div>
       </div>
     </div>
